32c467f490df687e26d503fd0b9a03e35e9cda6a
Aleks <example@gmail.com>
2023-05-23 23:20:57 +0200

    start film database

---
+++ index.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('How much films do you already watched ?', '');
+
+const personalMovieBD = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  geres: [],
+  privat: false,
+};
+
+const a = prompt('Type here one of the last film that you watch', ''),
+  b = +prompt('Please rate this film', ''),
+  c = prompt('Type here one of the last film that you watch', ''),
+  d = +prompt('Please rate this film', '');
+
+personalMovieBD.movies[a] = b;
+personalMovieBD.movies[c] = d;
+
+console.log(personalMovieBD);
