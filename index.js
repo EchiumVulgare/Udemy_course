bd06b2d028f075dfe3b279576d797e4b86216cb0
Aleks <example@gmail.com>
2023-05-24 00:05:41 +0200

    add testApp script

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const numberOfFilms = +prompt('How much films do you already watched ?', '');
+// const numberOfFilms = +prompt('How much films do you already watched ?', '');
 
-const personalMovieBD = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  geres: [],
-  privat: false,
-};
+// const personalMovieBD = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   geres: [],
+//   privat: false,
+// };
 
-const a = prompt('Type here one of the last film that you watch', ''),
-  b = +prompt('Please rate this film', ''),
-  c = prompt('Type here one of the last film that you watch', ''),
-  d = +prompt('Please rate this film', '');
+// const a = prompt('Type here one of the last film that you watch', ''),
+//   b = +prompt('Please rate this film', ''),
+//   c = prompt('Type here one of the last film that you watch', ''),
+//   d = +prompt('Please rate this film', '');
 
-personalMovieBD.movies[a] = b;
-personalMovieBD.movies[c] = d;
+// personalMovieBD.movies[a] = b;
+// personalMovieBD.movies[c] = d;
 
-console.log(personalMovieBD);
+// console.log(personalMovieBD);
---
+++ testApp.js
@@ -0,0 +1,18 @@
+const num = 50;
+switch (num) {
+  case 49:
+    console.log('Wrong');
+    break;
+
+  case 100:
+    console.log('Wrong');
+    break;
+
+  case 50:
+    console.log('Correct');
+    break;
+
+  default:
+    console.log('Not this time');
+    break;
+}
